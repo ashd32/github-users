88e36f62a1f245324b903f55a63f66a837e00e96
Ashley Donald <ashd32@gmail.com>
2019-02-03 01:57:14 -0500

    up and running

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import Form from './components/Form'
+import CardList from './components/CardList'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const [cards, setCards] = useState([])
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const addNewCard = cardInfo => {
+    setCards(cards.concat(cardInfo))
+  }
+
+  return (
+    <div>
+      <Form onSubmit={addNewCard} />
+      <CardList cards={cards} />
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)

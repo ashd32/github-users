5543fd144235e7213db314855ca8e76d433e6213
Ashley Donald <ashd32@gmail.com>
2019-02-05 12:24:39 -0500

    attempted to add hyperlink

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,30 @@
 import React from 'react'
 
 const Card = props => {
-  return (
-    <div style={{ margin: '1em' }}>
-      <img alt="avatar" style={{ width: '70px' }} src={props.avatar_url} />
-      <div>
-        <div style={{ fontWeight: 'bold' }}>{props.name}</div>
-        <div>{props.blog}</div>
-      </div>
-    </div>
-  )
+
+class HyperLink extends Card {
+
+  constructor(){
+      super();
+      this._goToURL = this._goToURL.bind(this.blog);
+  }
+
+  static propTypes = {
+    url: props.blog.isRequired,
+  }
 }
 
-export default Card
+    return(
+      <div style={{ margin: '1em' }}>
+        <img alt="avatar" style={{ width: '70px' }} src={props.avatar_url} />
+          <div>
+           <div style={{ fontWeight: 'bold' }}>{props.name}</div>
+             <div>{props.blog.url}</div>
+             <HyperLink />
+          </div>
+      </div>
+
+    );
+  }
+
+  export default Card
